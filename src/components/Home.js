f0526c3c97e807e71fdabaf75ed161e63f4fb2fc
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-09-22 12:48:33 -0400

    clean up and fresh start

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import data from '../data/data.json';
 
-const Home = (props) => {
-  return (
-    <div className="Grid animated bounceInUp">
-      {
-        props.cards.map((card) => (
-          <Card 
-            duration={150} 
-            key={card.id} 
-            card={card} 
-          />
-        ))
-      }
-    </div>
-  )
-}
+class Home extends Component {
+  state = { cards: data }
+  render() { 
+    return ( 
+      <div className="Grid animated bounceInUp">
+        {
+          this.state.cards.map((card) => (
+            <Card 
+              duration={150} 
+              key={card.id} 
+              card={card} 
+            />
+          ))
+        }
+      </div>
+     )
+  }
+};
  
 export default Home;
